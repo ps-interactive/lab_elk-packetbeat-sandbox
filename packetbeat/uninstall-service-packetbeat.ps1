3de670f79c1b790e266b2684cf3c8c198c8abdfa
treyescairo-pluralsight <64978363+treyescairo-pluralsight@users.noreply.github.com>
2023-09-28 13:39:20 -0600

    Just yml

--- packetbeat/uninstall-service-packetbeat.ps1
+++
@@ -1,7 +0,0 @@
-# Delete and stop the service if it already exists.
-if (Get-Service packetbeat -ErrorAction SilentlyContinue) {
-  Stop-Service packetbeat
-  (Get-Service packetbeat).WaitForStatus('Stopped')
-  Start-Sleep -s 1
-  sc.exe delete packetbeat
-}
